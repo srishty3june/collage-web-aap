e641ac8414a66eab0fdcbaddeff51dda8ed3ada1
srishty3june <93928989+srishty3june@users.noreply.github.com>
2022-09-16 16:53:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function speak() {
     setTimeout(function () {
         img_id = "selfie_1";
         take_snapshot();
-        speak_data = "taking your selfie in 5 seconds";
+        speak_data = "taking your selfie in 10 seconds";
         var utterThis = new SpeechSynthesisUtterance(speak_data);
         synth.speak(utterThis);
 
@@ -47,7 +47,7 @@ function speak() {
     setTimeout(function () {
         img_id = "selfie_2";
         take_snapshot();
-        speak_data = "taking your selfie in 10 seconds";
+        speak_data = "taking your selfie in 15 seconds";
         var utterThis = new SpeechSynthesisUtterance(speak_data);
         synth.speak(utterThis);
 
@@ -56,10 +56,7 @@ function speak() {
     setTimeout(function () {
         img_id = "selfie_3";
         take_snapshot();
-        speak_data = "taking your selfie in 15 seconds";
-        var utterThis = new SpeechSynthesisUtterance(speak_data);
-        synth.speak(utterThis);
-
+        
     }, 15000)
 
 }
@@ -68,18 +65,18 @@ function take_snapshot() {
 
     console.log(img_id);
 
-    webcam.snap(function (data_uri) {
+    Webcam.snap(function (data_uri) {
 
-        if (img_id == "selfir_1") {
-            document.getElementById(result_1).innerHTML = '<img id = "selfie_1" src="' + data_uri + '"/>';
+        if (img_id == "selfie_1") {
+            document.getElementById("result1").innerHTML = '<img id = "selfie_1" src="' + data_uri + '"/>';
         }
 
-        if (img_id == "selfir_2") {
-            document.getElementById(result_2).innerHTML = '<img id = "selfie_2" src="' + data_uri + '"/>';
+        if (img_id == "selfie_2") {
+            document.getElementById("result2").innerHTML = '<img id = "selfie_2" src="' + data_uri + '"/>';
         }
 
-        if (img_id == "selfir_3") {
-            document.getElementById(result_3).innerHTML = '<img id = "selfie_3" src="' + data_uri + '"/>';
+        if (img_id == "selfie_3") {
+            document.getElementById("result3").innerHTML = '<img id = "selfie_3" src="' + data_uri + '"/>';
         }
     });
 }
